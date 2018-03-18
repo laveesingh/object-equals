a28534b88e78424a2fe07e925c79a16524204c5b
laveesingh <lavees96@gmail.com>
2018-03-18 22:47:27 +0530

    added options like depth, keysOnly and valuesOnly
    fixes: #1
    fixes: #2
    fixes: #3

--- src/main.js
+++ src/main.js
@@ -1,17 +1,33 @@
 
-export const objectEquals = (object1, object2) => {
-  if (object1 === null || object2 === null ||
-    typeof object1 !== 'object' || typeof object2 !== 'object') {
+export const objectEquals = (object1, object2, options={}) => {
+    let currentDepth = 0
+    let depth = options.depth || 100000
+    let keysOnly = options.keysOnly || false
+    let valuesOnly = options.valuesOnly || false
+  return objectEqualsUtil(object1, object2, {
+    depth, currentDepth, keysOnly, valuesOnly
+  })
+}
+
+const objectEqualsUtil = (object1, object2, {
+  depth, currentDepth, keysOnly, valuesOnly
+}) => {
+  if (!object1 || !object2 || typeof object1 !== 'object' ||
+    typeof object2 !== 'object') {
     return object1 === object2
   }
+  if (currentDepth >= depth) { return true }
+  currentDepth += 1
   let entries1 = Object.entries(object1)
   let entries2 = Object.entries(object2)
   if (entries1.length !== entries2.length) { return false }
   for (let index = 0; index < entries1.length; index += 1) {
-    if (entries1[index][0] !== entries2[index][0]) { return false }
-    let valueComparison = objectEquals(entries1[index][1],
-      entries2[index][1])
-    if (!valueComparison) { return false }
+    if (entries1[index][0] !== entries2[index][0] &&
+      !valuesOnly) { return false }
+    let valueComparison = objectEqualsUtil(entries1[index][1],
+      entries2[index][1], {depth, currentDepth, keysOnly, valuesOnly})
+    if (!valueComparison && !keysOnly) { return false }
   }
   return true
 }
+
